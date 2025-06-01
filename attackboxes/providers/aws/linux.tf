e328a120c1df37fbe94dcd5b80b357cb4dfcec7d
orthrus1775 <orthrus1775@gmail.com>
2025-06-01 15:25:31 -0400

    Attackbox count update

--- attackboxes/providers/aws/linux.tf
+++ attackboxes/providers/aws/linux.tf
@@ -32,3 +32,25 @@
   size               = "t2.2xlarge"  # Include both instance_type and size
   volume_size        = 60  # Disk size in GB
 }
+"attackbox-4" = {
+  name               = "attackbox-4"
+  linux_sku          = "24_04-lts-gen2"
+  linux_version      = "latest"
+  ami                = "ami-0158cbc3c8e9ef377"
+  private_ip_address = "{{ip_range}}.50"
+  password           = "suppaP@ssw0rd$"
+  instance_type      = "t2.2xlarge"
+  size               = "t2.2xlarge"  # Include both instance_type and size
+  volume_size        = 60  # Disk size in GB
+}
+"attackbox-5" = {
+  name               = "attackbox-5"
+  linux_sku          = "24_04-lts-gen2"
+  linux_version      = "latest"
+  ami                = "ami-0158cbc3c8e9ef377"
+  private_ip_address = "{{ip_range}}.50"
+  password           = "suppaP@ssw0rd$"
+  instance_type      = "t2.2xlarge"
+  size               = "t2.2xlarge"  # Include both instance_type and size
+  volume_size        = 60  # Disk size in GB
+}
